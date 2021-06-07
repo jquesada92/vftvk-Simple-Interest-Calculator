812483509ca49557176942d1f328465e43e22b72
jquesada92 <42700058+jquesada92@users.noreply.github.com>
2021-06-06 23:57:10 -0500

    primer commit
    Modificacion del codigo original del cuerso.
    
    Agregue validaciones del monto a colocar(mayort a cero) e insercion de mensaje con los calculos,

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = Number(document.getElementById("principal").value);
+    if (principal <= 0) {
+        alert("Enter a positive number");
+    } else {}
+    var rate = Number(document.getElementById("rate").value);
+    var years = Number(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var ParentNode = document.getElementById("InterestForm")
+    if (document.contains(document.getElementById("message"))) {
+        document.getElementById("message").remove();
+    } else {}
+    var message = document.createElement("p");
+    message.setAttribute("id", "message");
+    message.innerHTML = "If you deposit " + principal + ",at an interest rate of " + rate + "%.<br>You will receive an amount of " + interest + ", in the year " + year;
+    var submitbtn = document.getElementById("submitbtn"); // Get the <ul> element to insert a new node
+    ParentNode.insertBefore(message, submitbtn)
+
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
