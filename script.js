d5316cc266eeda667b17f105bf9d07379983b3a7
jquesada92 <42700058+jquesada92@users.noreply.github.com>
2021-06-07 20:30:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 function compute() {
     var principal = Number(document.getElementById("principal").value);
-    if (principal <= 0) {
-        alert("Enter a positive number");
-        document.getElementById("principal").focus()
-    } else {}
+
     var rate = Number(document.getElementById("rate").value);
     var years = Number(document.getElementById("years").value);
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear() + parseInt(years);
     var ParentNode = document.getElementById("InterestForm")
-    if (document.contains(document.getElementById("message"))) {
-        document.getElementById("message").remove();
-    } else {}
-    document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,at an interest rate of <mark>" + rate + "%</mark>.<br>You will receive an amount of <mark>" + interest + "</mark>, in the year <mark>" + year + "</mark>";
-    var submitbtn = document.getElementById("submitbtn"); // Get the <ul> element to insert a new node
-    ParentNode.insertBefore(message, submitbtn)
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        document.getElementById("result").innerHTML = ""
+    } else {
+        document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,at an interest rate of <mark>" + rate + "%</mark>.<br>You will receive an amount of <mark>" + interest + "</mark>, in the year <mark>" + year + "</mark>";
+        var submitbtn = document.getElementById("submitbtn"); // Get the <ul> element to insert a new node
+        ParentNode.insertBefore(message, submitbtn)
+    }
+
 
 }
 
